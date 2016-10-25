d1f043e38d07d493ee083289aa2e32f8a8060354
sven4ask <sven@legalthings.io>
2016-10-25 14:50:00 +0200

    Add options to toc generation and add tests

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-'use strict'
+ 'use strict'
 
 const TOC = require('table-of-contents-json');
 const toc = new TOC;
 
 exports.handler = (event, context, callback) => {
-    if (!event || typeof event !== 'string') {
-        return callback('Context must be a string');
+    if (!event && !event.html) {
+        return callback('No event received');
     }
 
-    if (event.indexOf('[ table of contents ]') === -1) {
+    if (event.html.indexOf('[ table of contents ]') === -1) {
         return callback('Context must contain a "[ table of contents ]" placeholder');
     }
 
-    const json = toc.generateJSON(event);
-    const result = toc.generateHTML(json, event);
+    const json = toc.generateJSON(event.html, event.options);
+    const result = toc.generateHTML(json, event.html, event.options);
 
     callback(null, result);
 }
